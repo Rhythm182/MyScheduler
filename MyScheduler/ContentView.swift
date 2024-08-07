3b234a7f9ec9e807e94251165e170807cb821815
Rhythm182 <rhythm.india2005@gmail.com>
2024-08-06 19:47:23 -0500

    Initial Commit

---
+++ MyScheduler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyScheduler
+//
+//  Created by Rhythm Khandelwal on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyScheduler/MySchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySchedulerApp.swift
+//  MyScheduler
+//
+//  Created by Rhythm Khandelwal on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MySchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
